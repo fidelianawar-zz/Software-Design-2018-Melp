f1787b3249766f4ca6df24c990a86e7ebea26d54
awant <awant@DESKTOP-BQ068OP>
2018-12-13 16:11:27 -0700

    JavaFx Spike Story v2

--- SpikeStoryFX/Main.java
+++ SpikeStoryFX/Main.java
@@ -3,26 +3,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         // just load fxml file and display it in the stage:
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainUI.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    // main method to support non-JavaFX-aware environments:
-
-    public static void main(String[] args) {
+     // main method to support non-JavaFX-aware environments:
+     public static void main(String[] args) {
         // starts the FX toolkit, instantiates this class, 
         // and calls start(...) on the FX Application thread:
         launch(args); 
     }
-}
+} 

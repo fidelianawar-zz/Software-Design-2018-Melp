4ecc58046e6321ea97602778d75bc12b11826797
laurenstierman <laurenstierman@172.16.141.203>
2018-12-13 22:26:50 -0700

    Updated methods in Restaurant Review class

--- src/RestaurantReview.java
+++ src/RestaurantReview.java
@@ -3,6 +3,30 @@
 * The RestaurantReview class creates the type RestaurantReview.
 */
 public class RestaurantReview {
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getRestaurantUnderReview() {
+		return restaurantUnderReview;
+	}
+
+	public void setRestaurantUnderReview(String restaurantUnderReview) {
+		this.restaurantUnderReview = restaurantUnderReview;
+	}
+
 	private String content;
 	private int rating;
 	private String restaurantUnderReview;

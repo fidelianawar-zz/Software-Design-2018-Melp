c91393423ad20e12d654a12b45ca36d272d6c210
laurenstierman <laurenstierman@172.16.141.203>
2018-12-13 20:52:39 -0700

    Approve request for restaurant review

--- src/RestaurantReview.java
+++ src/RestaurantReview.java
@@ -1,6 +1,6 @@
 
 /**
-* The RestaurantReview class
+* The RestaurantReview class creates the type RestaurantReview.
 */
 public class RestaurantReview {
 	private String content;
@@ -8,12 +8,22 @@ public class RestaurantReview {
 	private Restaurant restaurantUnderReview;
 	private String[] vulgarWords = {"crappy", "stupid", "dumb", "idiot", "dummies", "loser", "fool", "ass", "imbecile"};
 	
+	/**
+	 * Constructor for the RestaurantReview class. Initializes instance variable of name.
+	 * @param the review content
+	 * @param the rating of the restaurant
+	 * @param the name of the restaurant under review
+	 */
 	public RestaurantReview(String content, int rating, Restaurant restaurantUnderReview) {
 		this.content = content;
 		this.rating = rating;
 		this.restaurantUnderReview = restaurantUnderReview;
 	}
 	
+	/**
+	 * Detemines whether a request should be approved or denied for vulgarity
+	 * @return true if the request is approved
+	 */
 	public boolean approveRequest() {
 		String[] words = content.split(" ");
 		for(String word: words) {
